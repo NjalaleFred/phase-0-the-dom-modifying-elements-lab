31fa301a205c0cb4515064a894a538e46c2a7b5d
Njalale <fnjalale@gmail.com>
2023-03-21 21:03:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector('main')
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Njalale is the champion'
+document.body.append(newHeader);
+
+//This one is another method I googled
+//newHeader.setAttribute('id', 'victory');
+//const hText = document.createTextNode('Njalale is the champion');
+//newHeader.appendChild(hText);
